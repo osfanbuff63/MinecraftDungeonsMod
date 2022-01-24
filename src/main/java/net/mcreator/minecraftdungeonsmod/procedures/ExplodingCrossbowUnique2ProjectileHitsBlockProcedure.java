0dd89094e8861498ca829b982e5998b6303d4835
osfanbuff63 <91388253+osfanbuff63@users.noreply.github.com>
2022-01-24 09:02:35 -0500

    Update ExplodingCrossbowUnique2ProjectileHitsBlockProcedure.java

--- src/main/java/net/mcreator/minecraftdungeonsmod/procedures/ExplodingCrossbowUnique2ProjectileHitsBlockProcedure.java
+++ src/main/java/net/mcreator/minecraftdungeonsmod/procedures/ExplodingCrossbowUnique2ProjectileHitsBlockProcedure.java
@@ -10,7 +10,7 @@ public class ExplodingCrossbowUnique2ProjectileHitsBlockProcedure {
 			Entity _ent = entity;
 			if (!_ent.level.isClientSide() && _ent.getServer() != null)
 				_ent.getServer().getCommands().performCommand(_ent.createCommandSourceStack().withSuppressedOutput().withPermission(4),
-						"execute at @e[type=arrow,sort=nearest,limit=1] run summon creeper ~ ~ ~ {Ignited:1}");
+						"execute at @e[type=arrow,sort=nearest,limit=1] run summon tnt");
 		}
 	}
 }
